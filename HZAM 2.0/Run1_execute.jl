de67b85950a43cfd466220ebb0b2c65dcb261d29
Darren Irwin <irwin@zoology.ubc.ca>
2024-01-31 15:22:49 -0800

    modified to print out number of threads

--- HZAM 2.0/Run1_execute.jl
+++ HZAM 2.0/Run1_execute.jl
@@ -5,4 +5,6 @@ using JLD2 # needed for saving / loading data in Julia format
 
 using Plots
 
+println("Number of threads is $(Threads.nthreads()) ")
+
 HZAM.run_HZAM_sets_complete("Run1_31Jan2024")
