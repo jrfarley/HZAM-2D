6181c52e763f5b12e48d1169762fb96d91c55164
jfarley1025 <jackfarley1025@gmail.com>
2025-07-22 17:59:21 -0400

    fixed Run3_execute

--- Run3_execute.jl
+++ Run3_execute.jl
@@ -19,9 +19,10 @@ end
 end
 
 # run main simulation sets
-HZAM.run_HZAM_sets_complete_nine_loci("Run3", set_numbers=[3])
+HZAM.run_HZAM_sets_complete_nine_loci("Run3_replicate_1", set_numbers=2:9)
+HZAM.run_HZAM_sets_complete_one_locus("Run3_replicate_1", set_numbers=3:9)
 
-for i in 1:4
+for i in 2:4
 	HZAM.run_HZAM_sets_complete_one_locus("Run3_replicate_$i")
 	HZAM.run_HZAM_sets_complete_nine_loci("Run3_replicate_$i")
 end
